12220e70a2bf5820f1e5ca0798e5a5b8d7ddd39b
danilsavc <danilsavc@gmail.com>
2022-12-03 12:57:43 +0200

    Created Pagination

--- src/Components/Shop/index.jsx
+++ src/Components/Shop/index.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import React from 'react';
+
 import { useSelector, useDispatch } from 'react-redux';
 
 import {setCategoryId} from '../../redux/slices/filterSlice'
+import Pagination from '../Pagination';
 
 import ShopItem from '../ShopItem';
 import Skeleton from '../ShopItem/Skeleton';
@@ -17,6 +19,7 @@ const Shop = () => {
 
   const [clothers, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [page, setPage] = React.useState(1)
 
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id))
@@ -24,40 +27,41 @@ const Shop = () => {
 
   React.useEffect(() => {
     setIsLoading(true)
-    axios.get(`https://63763d1481a568fc25f99127.mockapi.io/items?${categoryId > 0 ? `category=${categoryId}` : ""}`)
+    axios.get(`https://63763d1481a568fc25f99127.mockapi.io/items?page=${page}&limit=6&${categoryId > 0 ? `category=${categoryId}` : ""}`)
     .then((res) => {
       setItems(res.data)
       setIsLoading(false)
     })
-  }, [categoryId])
+  }, [categoryId, page])
 
   
 
   return (
-    <>
-      <div className={style.shop}>
-        <h1 className={style.title}>Магазин</h1>
-        <div className={style.subtitle}>
-          <span className={style.main}>Головна - </span> 
-          <span className={style.minor}>Магазин</span>
-        </div>
-
-        <div className={style.categories}>
-          <Categories value={categoryId} onChangeCategory={onChangeCategory}/>
-        </div>
-                
-
-        <div className={style.items}>
-          {isLoading 
-            ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>) 
-            : clothers.map((item, index) => (
-              <div key={index} className={style.container}>
-                <ShopItem girl={item.girl} price={item.price} title={item.title}/>
-              </div>))
-            }
-        </div>
+    <div className={style.shop}>
+      <h1 className={style.title}>Магазин</h1>
+      <div className={style.subtitle}>
+        <span className={style.main}>Головна - </span> 
+        <span className={style.minor}>Магазин</span>
       </div>
-    </>
+
+      <div className={style.categories}>
+        <Categories value={categoryId} onChangeCategory={onChangeCategory}/>
+      </div>
+              
+      <div className={style.items}>
+        {isLoading 
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>) 
+          : clothers.map((item, index) => (
+            <div key={index} className={style.container}>
+              <ShopItem girl={item.girl} price={item.price} title={item.title}/>
+            </div>))
+          }
+      </div>
+      <div className={style.pagination}>
+        <Pagination onChangePage={number => setPage(number)}/>
+      </div>
+      
+    </div>
   );
 };
 
