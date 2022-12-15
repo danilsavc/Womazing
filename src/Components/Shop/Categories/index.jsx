9c5b47874efbd387df1763aa9be1e3df625907e3
danilsavc <danilsavc@gmail.com>
2022-12-15 17:56:42 +0200

    Created adaptiv for Shop components

--- src/Components/Shop/Categories/index.jsx
+++ src/Components/Shop/Categories/index.jsx
@@ -4,13 +4,11 @@ const Categories = ({value, onChangeCategory}) => {
   const items = ['Всі', 'Пальто', 'Джинси', 'Блейзери', 'Світшоти']
 
   return (
-    <div>
-      <nav className={style.categories}>
-          {items.map((categoryName, index) => (
-            <div key={index} onClick={() => onChangeCategory(index)} className={value === index ? `${style.categories_item} ${style.active}` : `${style.categories_item}`}><span>{categoryName}</span></div>
-          ))}
-        </nav>
-    </div>
+    <nav className={style.categories}>
+        {items.map((categoryName, index) => (
+          <div key={index} onClick={() => onChangeCategory(index)} className={value === index ? `${style.categories_item} ${style.active}` : `${style.categories_item}`}><span>{categoryName}</span></div>
+        ))}
+    </nav>
   );
 };
 
